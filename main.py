36c12884d23fc8fde37cf5030248d12a2ac9994f
Illia <pulsatil1@gmail.com>
2025-03-14 10:22:45 +0200

    Add login functionality

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+def login(username, password):
+    return True
+
 def main():
+    if login("admin", "1234"):
+	print("Login successfull!")
+    else:
+	print("Login failed!")
+
     print("Hello, World!")
     return 0
 
